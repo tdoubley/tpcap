4f41ee3f73204f7e76de481b8c8f599028313879
toubley <toubley@163.com>
2018-12-29 20:25:40 +0800

    添加http相关数据结构

---
+++ src/protocol/http.h
@@ -0,0 +1,25 @@
+#ifndef _SRC_PROTOCOL_HTTP_H_
+#define _SRC_PROTOCOL_HTTP_H_
+
+
+typedef struct str {
+    u_char *data;
+    unsigned len;
+}str_t;
+
+typedef struct http_header {
+    str_t name;
+    str_t value;
+    struct http_header *next;
+}http_header_t;
+
+/* HTTP */
+typedef struct http {
+    str_t uri;
+    http_header_t *headers;
+    str_t body;
+}http_t;
+
+
+
+#endif // _SRC_PROTOCOL_HTTP_H_
